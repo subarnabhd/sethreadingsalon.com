6e421544adcc11cbc783ba83b34d371938fd6406
Subarna Bhandari <subarnabhd@Subarnas-MacBook-Pro.local>
2024-06-01 22:46:13 +0545

    update

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     return (
 
 
+
         <nav className="header sticky top-0 bg-primary bg-primary border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
 
@@ -21,21 +22,21 @@ const Navbar = () => {
                 </button>
 
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x rtl:space-x-reverse md:mt-0 md:border-0">
                         <li>
-                            <Link href="/" className="block py-10 px-12 text-white c-secondary rounded-full md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500  uppercase">Home</Link>
+                            <Link href="/" className="block px-5 py-3 text-sm rounded-full text-white uppercase">Home</Link>
                         </li>
                         <li>
-                            <Link href="/about" className="block py-8 px-12  c-secondary rounded-full hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700   md:dark:hover:bg-transparent uppercase">About</Link>
+                            <Link href="/about" className="block px-5 py-3 text-sm rounded-full text-white uppercase">About</Link>
                         </li>
                         <li>
-                            <Link href="/services" className="block py-8 px-12 text-gray-900 rounded-full hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent uppercase">Services</Link>
+                            <Link href="/services" className="block px-5 py-3 text-sm rounded-full text-white uppercase">Services</Link>
                         </li>
                         <li>
-                            <Link href="/gallery" className="block py-8 px-12 text-gray-900 rounded-full hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent uppercase">Gallery</Link>
+                            <Link href="/gallery" className="block px-5 py-3 text-sm rounded-full text-white uppercase">Gallery</Link>
                         </li>
                         <li>
-                            <Link href="/contact" className="block py-8 px-12 text-gray-900 rounded-full hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent uppercase">Contact</Link>
+                            <Link href="/contact" className="block px-5 py-3 text-sm rounded-full text-white uppercase">Contact</Link>
                         </li>
                     </ul>
                 </div>
